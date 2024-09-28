6cef5f4abf43e7b9cf3f52871b719a2d13f86b47
Anton Mozgovoy <anton_mozgovoy@inbox.ru>
2024-09-28 19:51:10 +0300

    Переименовал класс и тест, копипаста коварная вещь)))

--- tests/test_list_orders.py
+++ tests/test_list_orders.py
@@ -3,10 +3,10 @@ import requests
 from urls import Urls
 
 
-class TestLoginCourier:
+class TestOrderList:
     @allure.title('Проверка возврата списка заказов')
     @allure.description('Проверяем успешный ответ и наличие списка заказов')
-    def test_login_courier(self):   # успешная авторизация курьера
-        login_request = requests.get(Urls.BASE_URL + Urls.LIST_ORDERS)
-        r = login_request.json()
-        assert login_request.status_code == 200 and r["orders"][0]
+    def test_order_list(self):   # успешная авторизация курьера
+        order_list = requests.get(Urls.BASE_URL + Urls.LIST_ORDERS)
+        response = order_list.json()
+        assert order_list.status_code == 200 and response["orders"][0]
